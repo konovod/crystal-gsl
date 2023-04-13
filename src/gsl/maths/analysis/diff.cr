24d92383f4b2c81b7e05d5d7b6cfc55943c93efc
konovod <konovod@gmail.com>
2023-04-13 21:09:54 +0300

    inline docs for `diff`

--- src/gsl/maths/analysis/diff.cr
+++ src/gsl/maths/analysis/diff.cr
@@ -1,10 +1,32 @@
 module GSL
+  # This enum represents algorithm of [Numerical Differentiation](https://www.gnu.org/software/gsl/doc/html/diff.html)
   enum Diff::Direction
+    # Function will be evaluated at points `x-step`, `x-step/2`, `x+step/2` and `x+step`
+    # This is default option
     Central
+    # Function will be evaluated at points `x+step/4`, `x+step/2`, `x+3*step/4` and `x+step`
+    # It is useful when function is not defined at points <= x
     Forward
+    # Equivalent to `Forward` but with negative `step`
     Backward
   end
 
+  # This function implements [Numerical Differentiation](https://www.gnu.org/software/gsl/doc/html/diff.html)
+  #
+  # This is a variant of method where function is passed as pointer
+  #
+  # Usage example:
+  # ```
+  # f = ->(x : Float64) { Math.log(x) }
+  # result, eps = GSL.diff(f, 4.0)
+  # result.should be_close 0.25, 1e-9
+  # ```
+  #
+  # Parameters:
+  #  - `function` - pointer to function
+  #  - `x` - point where differential is calculated
+  #  - `step` - step of algorithm. Function values will be evaluated in a range `(x-step .. x+step)`.
+  #  - `dir` - algorithm. See `Diff::Direction` and GSL docs for details
   def self.diff(function : GSL::Function, x : Float64, step : Float64 = 0.01, dir : Diff::Direction = Diff::Direction::Central)
     f = wrap_function(function)
     result = uninitialized Float64
@@ -23,6 +45,21 @@ module GSL
     return result, abserr
   end
 
+  # This function implements [Numerical Differentiation](https://www.gnu.org/software/gsl/doc/html/diff.html)
+  #
+  # This is a variant of method where function is passed as a block
+  #
+  # Usage example:
+  # ```
+  # result, eps = GSL.diff(0.25) { |x| Math.log(x) }
+  # result.should be_close 4.0, 1e-9
+  # ```
+  #
+  # Parameters:
+  #  - `function` - pointer to function
+  #  - `x` - point where differential is calculated
+  #  - `step` - step of algorithm. Function values will be evaluated in a range `(x-step .. x+step)`.
+  #  - `dir` - algorithm. See `Diff::Direction` and GSL docs for details
   def self.diff(x : Float64, step : Float64 = 0.01, dir : Diff::Direction = Diff::Direction::Central, &f : GSL::Function)
     diff(f, x, step, dir)
   end
