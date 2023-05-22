acfc3f17963eb5294033eb8706e73cdc10ceca0e
konovod <konovod@gmail.com>
2023-05-22 17:16:44 +0300

    more spec for LinearRegression

--- spec/fit_spec.cr
+++ spec/fit_spec.cr
@@ -37,5 +37,30 @@ describe GSL do
       y1.should be_close 201, 20
       y2.should be_close 201, 1
     end
+
+    it "could be created with zero c0" do
+      x = Statistics.linspace(0, 10, 100)
+      y = x.map { |v| v*2 + 1 }
+      lin1 = GSL.linreg(x, y)
+      lin2 = GSL.linreg(x, y, no_const: true)
+      y1, err1 = lin1.estimate(10.0)
+      y2, err2 = lin2.estimate(10.0)
+      y2.should be_close y1, 1.0
+      err2.should be > err1
+    end
+
+    it "could be created with zero c0 and weighted inputs" do
+      x = Statistics.linspace(0, 10, 100)
+      y = x.map { |v| v*2 + 1 }
+      lin1 = GSL.linreg(x, y, no_const: true)
+      y[2] = 20.0
+      w = Array.new(x.size, 1.0)
+      w[2] = 100.0
+      lin2 = GSL.linreg(x, y, w, no_const: true)
+      y1, err1 = lin1.estimate(10.0)
+      y2, err2 = lin2.estimate(10.0)
+      y2.should be_close y1, 2.0
+      err2.should be > err1
+    end
   end
 end
